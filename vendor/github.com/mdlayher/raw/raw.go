d949970a659a55824a98a9da0a8e5570a49def06
Clément Duquenne <clement.duquenne.ext@orange.com>
2022-04-13 15:40:47 +0200

    Update go from 1.17 to 1.18 and bump dependencies

--- vendor/github.com/mdlayher/raw/raw.go
+++ vendor/github.com/mdlayher/raw/raw.go
@@ -1,5 +1,22 @@
-// Package raw enables reading and writing data at the device driver level for
-// a network interface.
+// Package raw enables reading and writing data at the device driver level for a
+// network interface.
+//
+// Deprecated: use github.com/mdlayher/packet on Linux instead. This package is
+// unmaintained.
+//
+//
+// Unmaintained
+//
+// This repository was one of my first major Go networking libraries. Although I
+// have updated it on Linux to incorporate modern Go best practices
+// (asynchronous I/O, runtime network poller integration), the non-Linux
+// platform code is effectively unmaintained and does not have the same level of
+// functionality.
+//
+// I encourage all Linux users of this package to migrate to
+// https://github.com/mdlayher/packet, which is a modern AF_PACKET library. The
+// existing *raw.Conn APIs now call directly into the equivalent *packet.Conn
+// APIs, and a level of indirection can be removed by migrating to that package.
 package raw
 
 import (
